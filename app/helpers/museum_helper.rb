269734ef70e271ef2ebbc7357d6ffc592cb4b52b
Joaquín Vicente <joakin@gmail.com>
2013-05-12 16:53:44 -0300

    quickfix: dummy images

--- app/helpers/museum_helper.rb
+++ app/helpers/museum_helper.rb
@@ -1,18 +1,20 @@
 module MuseumHelper
 
   def photo_tag museum, size = :full, options = {}
+    id = museum.id % 10
     if size == :thumb
-      image_tag "museums/thumb/#{museum.id}.jpg", options
+      image_tag "museums/thumb/#{id}.jpg", options
     else
-      image_tag "museums/full/#{museum.id}.jpg", options
+      image_tag "museums/full/#{id}.jpg", options
     end
   end
 
   def photo_path museum, size = :full
+    id = museum.id % 10
     if size == :thumb
-      asset_path "museums/thumb/#{museum.id}.jpg"
+      asset_path "museums/thumb/#{id}.jpg"
     else
-      asset_path "museums/full/#{museum.id}.jpg"
+      asset_path "museums/full/#{id}.jpg"
     end
   end
 
