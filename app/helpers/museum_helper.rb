c1c7ad6564dff24b11562c943c765d340644948f
Joaquín Vicente <joakin@gmail.com>
2013-05-12 16:41:49 -0300

    tag helper for photos

--- app/helpers/museum_helper.rb
+++ app/helpers/museum_helper.rb
@@ -1,2 +1,19 @@
 module MuseumHelper
+
+  def photo_tag museum, size = :full, options = {}
+    if size == :thumb
+      image_tag "museums/thumb/#{museum.id}.jpg", options
+    else
+      image_tag "museums/full/#{museum.id}.jpg", options
+    end
+  end
+
+  def photo_path museum, size = :full
+    if size == :thumb
+      asset_path "museums/thumb/#{museum.id}.jpg"
+    else
+      asset_path "museums/full/#{museum.id}.jpg"
+    end
+  end
+
 end
