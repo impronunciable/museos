5483629293084185670f6829490a5dd0baa10b24
Joaquín Vicente <joakin@gmail.com>
2013-05-12 17:50:05 -0300

    Changed Twitter/Instagram search tags

--- app/assets/javascripts/museos.js
+++ app/assets/javascripts/museos.js
@@ -13,7 +13,7 @@
 		$(this).siblings().removeClass('active');
 	});
 
-	$.getJSON('https://search.twitter.com/search.json?q=messi&callback=?', function(data){
+	$.getJSON('https://search.twitter.com/search.json?q=museos%20buenos%20aires&callback=?', function(data){
 		var html = "";
 		for(var i = 0; i < 6; i++) {
 			var el = data.results[i];
@@ -22,7 +22,7 @@
 		$('#twitter ul').html(html);
 	});
 
-	$.getJSON('https://api.instagram.com/v1/tags/olympics/media/recent?client_id=871310a665024ceb9a0a90e83ac4daed&callback=?', function(data){
+	$.getJSON('https://api.instagram.com/v1/tags/museum/media/recent?client_id=871310a665024ceb9a0a90e83ac4daed&callback=?', function(data){
 		var html = "";
 		for(var i = 0; i < 12; i++) {
 			var el = data.data[i];
