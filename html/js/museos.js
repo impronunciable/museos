14a52258bf540339d6f86c52808c3911414f3113
Ignacio Ricci <ignacio.ricci@gmail.com>
2013-05-18 18:42:53 -0300

    more styles

--- html/js/museos.js
+++ html/js/museos.js
@@ -5,7 +5,7 @@ $.getJSON('https://search.twitter.com/search.json?q=messi&callback=?', function(
 	var html = "";
 	for(var i = 0; i < 3; i++) {
 		var el = data.results[i];
-		html += "<li><a href='#'><img src='"+el.profile_image_url+"'></a><p>"+el.text+"</p><span>"+el.created_at+"</span></li>"
+		html += "<li><span class='twPic'><img src='"+el.profile_image_url+"'></span><span class='twText'>"+el.text+"<em>"+el.created_at+"</em></span></li>"
 	}
 
 	$('#twitter ul').html(html);
@@ -14,7 +14,7 @@ $.getJSON('https://search.twitter.com/search.json?q=messi&callback=?', function(
 
 $.getJSON('https://api.instagram.com/v1/tags/olympics/media/recent?client_id=871310a665024ceb9a0a90e83ac4daed&callback=?', function(data){
 	var html = "";
-	for(var i = 0; i < 6; i++) {
+	for(var i = 0; i < 12; i++) {
 		var el = data.data[i];
 		html += "<li><a href='#' data-big='"+el.images.standard_resolution.url+"'><img src='"+el.images.thumbnail.url+"'></a></li>"
 	}
